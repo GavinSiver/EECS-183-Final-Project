56f88c9d2cd4d37eb0433bc52a062e34195ffeae
Gavin Siver <GavinSiver@gmail.com>
2017-11-18 06:26:03 -0500

    Hello World Python File to Test Python Configuration

---
+++ Hello World.py
@@ -0,0 +1,6 @@
+__author__ = 'Gavin Siver'
+print ('Hello World')
+# print 'Hi Gavin. How are you? I am ' 1'.' .format(good)
+greeting = "Hello"
+name = raw_input("Please enter your name ")
+print(greeting + ' ' + name)
