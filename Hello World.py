5681d172a11163e741e019e10983c1d788a7890d
Gavin Siver <GavinSiver@gmail.com>
2017-11-18 19:21:10 -0500

    Hello World Testing File. Use to detrmine if Python 2.7 & some of its basic modules are operating correctly

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,16 @@
 __author__ = 'Gavin Siver'
-print ('Hello World')
-# print 'Hi Gavin. How are you? I am ' 1'.' .format(good)
-greeting = "Hello"
-name = raw_input("Please enter your name ")
-print(greeting + ' ' + name)
+print ('Hello World Test Program \n \n')
+
+name = 'Gavin'
+greeting = 'Hello'
+print ('Hi. I am %s. Who are you?' % name)
+name = raw_input('Please enter your name: ')
+print(greeting + ' ' + name + '.')
+
+age = raw_input('Please enter your age (numbers only please): ')
+print('Your age is %d years. \n' % int(age))
+
+for i in range(1, 12):
+    print('No. %2d squared is %4d and cubed is %4d' % (i, i ** 2, i ** 3))
+
+print('Pi is approximately %1.2f' % (22.0 / 7))
